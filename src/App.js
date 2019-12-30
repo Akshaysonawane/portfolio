2115522346e673d30268ec4ed15b318049d5ecaa
akshays0nawan3 <akshays0nawan3@gmail.com>
2019-12-31 00:12:09 +0530

    Resolved useStyle hook error and working on side menu

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+import SideMenu from './components/sideMenu/sideMenu'; 
+
+function App() {
+  return (
+    <div className="App">
+      <SideMenu></SideMenu>
+    </div>
+  );
+}
+
+export default App;
